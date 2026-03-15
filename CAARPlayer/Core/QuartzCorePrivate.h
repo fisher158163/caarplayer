62d763ef418d02a00ad03d670abaceb2756fa20e
Fisher Lee <fisherlee0820@outlook.com>
2026-03-16 00:05:12 +0800

    优化QuartzCorePrivate.h
    优化QuartzCorePrivate.h

--- CAARPlayer/Core/QuartzCorePrivate.h
+++ CAARPlayer/Core/QuartzCorePrivate.h
@@ -9,35 +9,25 @@
 @import Cocoa;
 @import QuartzCore;
 
-@interface CAFilter : NSObject <NSCopying, NSMutableCopying, NSCoding>
-
-+ (instancetype)filterWithType:(NSString *)type;
-+ (NSArray <NSString *> *)filterTypes;
-
-- (NSArray <NSString *> *)outputKeys;
-- (NSArray <NSString *> *)inputKeys;
-- (void)setDefaults;
-
-@property(copy) NSString *name;
-@property(readonly) NSString *type;
-
-@property(getter=isEnabled) BOOL enabled;
-
-@end
-
 extern NSData *CAEncodeLayerTree(CALayer *rootLayer);
-
 extern NSString *kCAPackageTypeArchive;
 extern NSString *kCAPackageTypeCAMLBundle;
 
-@interface CAPackage : NSObject
+@interface CAFilter : NSObject <NSCopying, NSMutableCopying, NSCoding>
++ (instancetype)filterWithType:(NSString *)type;
++ (NSArray <NSString *> *)filterTypes;
+- (NSArray <NSString *> *)outputKeys;
+- (NSArray <NSString *> *)inputKeys;
+- (void)setDefaults;
+@property(copy) NSString *name;
+@property(readonly) NSString *type;
+@property(getter=isEnabled) BOOL enabled;
+@end
 
+@interface CAPackage : NSObject
 + (id)packageWithData:(NSData *)data type:(NSString *)type options:(id)opts error:(NSError **)outError;
 + (id)packageWithContentsOfURL:(NSURL *)url type:(NSString *)type options:(id)opts error:(NSError **)outError;
-
 - (NSArray <NSString *> *)publishedObjectNames;
-
 @property(readonly, getter=isGeometryFlipped) BOOL geometryFlipped;
 @property(readonly) CALayer *rootLayer;
-
 @end
